cddaf9a82bdeda1a6d5860330815b1238bd836b3
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-19 20:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function changingText() {
 	let pElement=document.getElementById("status");
-	pElement.textContent="Entered Metaverse"
+	pElement.innerText="Entered Metaverse"
 
 	
 }
